03ea82e3a69dc3c54ace3a446a5a8af5cb5a838e
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-03-03 22:51:00 -0500

    draw list

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
 
-    SIDE_PAD = 100
-    TOP_PAD = 150
+    GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+
+    SIDE = 100
+    TOP = 150
 
 
     def __init__(self, width, height, lst):
@@ -28,9 +34,9 @@ class DrawInformation:
         self.max_val = max(lst)
         self.min_val = min(lst)
 
-        self.pixel_width = round((self.width - self.SIDE_PAD) / len(lst))
-        self.pixel_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
-        self.start_x = self.SIDE_PAD // 2
+        self.pixel_width = round((self.width - self.SIDE) / len(lst))
+        self.pixel_height = round((self.height - self.TOP) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE // 2
 
 def generate_starting_list(n, min_val, max_val):
     lst = []
@@ -40,6 +46,22 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(val)
 
     return lst
+def draw(info):
+    info.window.fill(info.BACKGROUND_COLOR) #override color (reset)
+    draw_list(info)
+    pygame.display.update()
+
+def draw_list(info):
+    lst = info.lst
+
+    for i, val in enumerate(lst):
+        x = info.start_x + 1 * info.block_width
+        y = info.height - (val - info.min_val)* info.block_height
+
+        color = info.GRADIENTS[i % 3]
+        
+        pygame.draw.rect(info.window, color, (x ,y , info.block_width, info.height))
+
 
 def main():
     run = True
@@ -53,7 +75,7 @@ def main():
     draw_info = DrawInformation(600, 400, lst)
     while run:
         clock.tick(60)
-
+        draw(draw_info) #resets background
         
         pygame.display.update()
 
